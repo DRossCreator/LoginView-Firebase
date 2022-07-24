f0458893c354a438a12145e3e14ce97902081688
YarkovenkoDev <99531855+YarkovenkoDev@users.noreply.github.com>
2022-07-24 16:16:08 +0500

    Add project.

---
+++ LoginView-Firebase/LoginView-Firebase/Sources/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  LoginView-Firebase
+//
+//  Created by Daniil Yarkovenko on 24.07.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
---
+++ LoginView-Firebase/LoginView-Firebase/Sources/Scenes/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  LoginView-Firebase
+//
+//  Created by Daniil Yarkovenko on 24.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
